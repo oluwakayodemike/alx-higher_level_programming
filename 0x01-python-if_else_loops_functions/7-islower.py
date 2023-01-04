f1e83ff5b8690d5558fcd98c25f7473757bd6a73
Oluwakayode Michael <michaelaykay@gmail.com>
2023-01-04 04:39:46 -0800

    7 islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+islower = __import__('7-islower').islower
+
+print("a is {}".format("lower" if islow("a") else "upper"))
+print("H is {}".format("lower" if islow("H") else "upper"))
+print("A is {}".format("lower" if islow("A") else "upper"))
+print("3 is {}".format("lower" if islow("3") else "upper"))
+print("g is {}".format("lower" if islow("g") else "upper"))
