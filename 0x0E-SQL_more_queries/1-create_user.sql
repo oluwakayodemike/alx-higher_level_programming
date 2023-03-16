11fb4cfa4e049b299ca588c75370404580d20f6e
Oluwakayode Michael <michaelaykay@gmail.com>
2023-03-16 16:18:53 -0700

    1. Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Enter your MySQL root password when prompted
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
