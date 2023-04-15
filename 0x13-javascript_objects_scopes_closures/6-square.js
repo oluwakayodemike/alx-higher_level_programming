18cf435805c930cad190a429b405f27d8c431ae8
Oluwakayode Michael <michaelaykay@gmail.com>
2023-04-14 19:24:02 -0700

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const Square = require('./5-square');
+
+class SquareWithCharPrint extends Square {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = SquareWithCharPrint;
+
