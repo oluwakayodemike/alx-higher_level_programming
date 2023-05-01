c2897db20bfd373cebcd8240aaa34834528b11dd
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-01 12:08:55 -0700

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""sscript that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8).
+"""
+Module for sending a request to a given URL and printing the response body.
 """
 
 import urllib.request
 import urllib.error
 import sys
 
-url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
