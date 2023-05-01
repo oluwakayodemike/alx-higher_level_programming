db6befecc044e2e29171efeb8e46fc50f7b1c60b
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-01 14:59:42 -0700

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Function that finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
     """ Find peak in list of unsorted integers """
     if not list_of_integers:
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     print(find_peak([2, 2, 2]))
     print(find_peak([]))
     print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1])) 
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
