92d3ece16801ce07761f641c1f1ffa6018f07d49
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-01 15:12:04 -0700

    8. cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script must send a POST request with the contents of a file, passed with
-# the filename as the second argument of the script, in the body of the
-# request.
+#script must send a POST request with the contents
 curl - sX POST - H "Content-Type: application/json" - d @ "$2" "$1"
