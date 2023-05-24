1da5563f402ef860176136730b2dad06bdf8f759
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-24 23:25:15 +0000

    8. Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characterUrls = movie.characters;
+    let charactersProcessed = 0;
+
+    characterUrls.forEach((characterUrl) => {
+      request.get(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+          charactersProcessed++;
+
+          if (charactersProcessed === characterUrls.length) {
+            // All characters processed
+            process.exit();
+          }
+        }
+      });
+    });
+  }
+});
+
