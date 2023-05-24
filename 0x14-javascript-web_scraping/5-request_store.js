af16a5cc4df76ae127697c2913c45ac6f4532fc8
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-24 23:20:00 +0000

    5. Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (error) => {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});
+
