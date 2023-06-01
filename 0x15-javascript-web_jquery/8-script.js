9d87ef5df91448e502643b2dd90f1c6c3d453db5
Oluwakayode Michael <michaelaykay@gmail.com>
2023-06-01 16:56:14 -0700

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+    var movies = data.results;
+    var list = $('#list_movies');
+    
+    for (var i = 0; i < movies.length; i++) {
+      var title = movies[i].title;
+      var listItem = $('<li>').text(title);
+      list.append(listItem);
+    }
+  });
+});
+
